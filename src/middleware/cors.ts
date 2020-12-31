869e3cb4330051cc9c7ff91e1361408d97ea46da
Leondro Lio <github@leondrolio.com>
2020-12-31 04:13:21 +0000

    Fix CORS

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,17 +1,25 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { parseIfValidFrontendOrigin } from "../helpers/isValid";
 
-const cors = (req: Request, res: Response) => {
+const cors = (req: Request, res: Response, next: NextFunction) => {
   const validDetails = parseIfValidFrontendOrigin(req.headers.origin);
 
   if (validDetails) {
     res.header("Access-Control-Allow-Origin", validDetails.origin);
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS, DELETE");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, Content-Type, Accept"
     );
+  }
+
+  // Check if it's an OPTIONS query
+  if (req.method === 'OPTIONS') {
+    res.send()
+  } else {
+    next();
   }
 }
 
